ed8ac1faa58a644b7755ecaddc56bb4b9106e056
shawal-mbalire <mbalireshawal@gmail.com>
2026-04-30 15:36:14 +0300

    feat: enhance CLI with auto-init and populate modules with boilerplate #18

---
+++ target/debug/deps/tokio_macros-4a38bd9b69e822ea.d
@@ -0,0 +1,7 @@
+/home/shawal/GitHub/shastack/target/debug/deps/tokio_macros-4a38bd9b69e822ea.d: /home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/lib.rs /home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/entry.rs /home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/select.rs
+
+/home/shawal/GitHub/shastack/target/debug/deps/libtokio_macros-4a38bd9b69e822ea.so: /home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/lib.rs /home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/entry.rs /home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/select.rs
+
+/home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/lib.rs:
+/home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/entry.rs:
+/home/shawal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.7.0/src/select.rs:
